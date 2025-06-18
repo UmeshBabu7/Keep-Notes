d11fa61f708f59ca7c19c2b2cd999c1d4ef88205
Umesh <umeshtamang129@gmail.com>
2025-06-18 12:32:27 +0545

    improved the UI

--- keepproject/notes/urls.py
+++ keepproject/notes/urls.py
@@ -3,6 +3,8 @@ from . import views
 import django.contrib.auth.views as auth_views
 
 app_name="notes"
+
+app_name="notes"
 urlpatterns = [
     path('',views.index,name='index'),
     path('login/',auth_views.LoginView.as_view(template_name='login.html'),name='login'),
